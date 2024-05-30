688e6734f6be9349524486d4fd2c617b6cfd6e18
davillatoro <davillatoro@scl.edu.gt>
2024-05-30 12:30:04 -0600

    Header_y_footer_realizados

--- footer/footer.js
+++ footer/footer.js
@@ -1,3 +1,14 @@
 let footer_DOM = document.querySelector(".footer");
 
-footer_DOM.innerHTML= `Soy footer`;
+footer_DOM.innerHTML= `
+<footer>
+<img src="https://pbs.twimg.com/profile_images/3537494309/7f65493e0976d78e4970df97a5b40f10_400x400.png" alt="">
+<h3>@davillatoro</h3>
+<div>
+<h3>Colaboradores</h3>
+<a href="https://github.com/xalyrodas">ALison Rodas</a>
+<a href="https://github.com/Rg4Pablo21">Pablo Giron</a>
+<a href="https://github.com/Saajvatz">Samuel Ajvatz</a>
+</div>
+</footer>
+`;
--- header/header.js
+++ header/header.js
@@ -1,3 +1,6 @@
 let header_DOM = document.querySelector(".header");
 
-header_DOM.innerHTML= `Soy header`;
+header_DOM.innerHTML= 
+`
+<header> <h1>Gramatica HTML</h1></header> <a href="https://github.com/elmojondesatan/Etiquetas_html" class="btn_git">Github </a>
+`;
