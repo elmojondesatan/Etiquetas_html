0f270f2657fae2bf91f070ce527e82259349959d
davillatoro <davillatoro@scl.edu.gt>
2024-05-30 10:30:40 -0600

    Maquetacion_del_proyecto

---
+++ footer/footer.js
@@ -0,0 +1,3 @@
+let footer_DOM = document.querySelector(".footer");
+
+footer_DOM.innerHTML= `Soy footer`;
---
+++ header/header.js
@@ -0,0 +1,3 @@
+let header_DOM = document.querySelector(".header");
+
+header_DOM.innerHTML= `Soy header`;
